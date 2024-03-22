671bec4a4ecedb07712dfe952473fbbbe84a5141
Krish-Debroy <debroy.krish@gmail.com>
2024-03-22 11:54:49 +0530

    GUI change in admin resource check page

--- Admin_frontend.py
+++ Admin_frontend.py
@@ -59,48 +59,48 @@ def check_resources_page(window):
     page_frame=Frame(window)
     display_df=pd.read_csv(resources_file)
     display_frame=Frame(page_frame)
-    Label(display_frame,text="Resource Utilisation  Information ",font=('Courier New Greek',22,'underline','bold')).grid(row=0,column=0,columnspan=2,pady=(0,10))
+    Label(display_frame,text="Resource Utilisation  Information ",font=('Courier New Greek',22,'underline','bold')).grid(row=0,column=0,columnspan=4,pady=(0,10))
     resource_list_var=[StringVar()]
     resource_list_var[0].set("Resource Type")
     resource_list_entry=[Entry(display_frame,textvariable=resource_list_var[0])]
     resource_list_entry[0].config(state='disabled')
-    resource_list_entry[0].grid(row=0,column=0)
+    resource_list_entry[0].grid(row=1,column=0)
     type_list_var=[StringVar()]
     type_list_var[0].set("Resource Name")
     type_list_entry=[Entry(display_frame,textvariable=type_list_var[0])]
     type_list_entry[0].config(state='disabled')
-    type_list_entry[0].grid(row=0,column=1)
+    type_list_entry[0].grid(row=1,column=1)
     num_available_var=[StringVar()]
     num_available_var[0].set("Total Number Available")
     num_available_entry=[Entry(display_frame,textvariable=num_available_var[0])]
     num_available_entry[0].config(state='disabled')
-    num_available_entry[0].grid(row=0,column=2)
+    num_available_entry[0].grid(row=1,column=2)
     num_in_use_var=[StringVar()]
     num_in_use_var[0].set("Number In Use")
     num_in_use_entry=[Entry(display_frame,textvariable=num_in_use_var[0])]
     num_in_use_entry[0].config(state='disabled')
-    num_in_use_entry[0].grid(row=0,column=3)
+    num_in_use_entry[0].grid(row=1,column=3)
     for i in display_df.index:
         resource_list_var.append(StringVar())
         resource_list_var[i+1].set(str(display_df.iat[i,0]))
         resource_list_entry.append(Entry(display_frame,textvariable=resource_list_var[i+1]))
         resource_list_entry[i+1].config(state='disabled')
-        resource_list_entry[i+1].grid(row=i+1,column=0)
+        resource_list_entry[i+1].grid(row=i+2,column=0)
         type_list_var.append(StringVar())
         type_list_var[i+1].set(str(display_df.iat[i,1]))
         type_list_entry.append(Entry(display_frame,textvariable=type_list_var[i+1]))
         type_list_entry[i+1].config(state='disabled')
-        type_list_entry[i+1].grid(row=i+1,column=1)
+        type_list_entry[i+1].grid(row=i+2,column=1)
         num_available_var.append(StringVar())
         num_available_var[i+1].set(str(display_df.iat[i,2]))
         num_available_entry.append(Entry(display_frame,textvariable=num_available_var[i+1]))
         num_available_entry[i+1].config(state='disabled')
-        num_available_entry[i+1].grid(row=i+1,column=2)
+        num_available_entry[i+1].grid(row=i+2,column=2)
         num_in_use_var.append(StringVar())
         num_in_use_var[i+1].set(str(display_df.iat[i,3]))
         num_in_use_entry.append(Entry(display_frame,textvariable=num_in_use_var[i+1]))
         num_in_use_entry[i+1].config(state='disabled')
-        num_in_use_entry[i+1].grid(row=i+1,column=3)
+        num_in_use_entry[i+1].grid(row=i+2,column=3)
     display_frame.pack()
     page_frame.pack()
 
