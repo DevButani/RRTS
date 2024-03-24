5ba439259fad8e4f96231865c2f4f57215246c20
DevButani <devbutani26@gmail.com>
2024-03-24 15:33:58 +0530

    modified Clerk, Login, Supervisor

--- Clerk.py
+++ Clerk.py
@@ -1,33 +1,37 @@
 import pandas as pd
-import tkinter as tk
 from tkinter import *
 from datetime import date
-
-
 
 def commit_changes(file_name,append_df):
     append_df.to_csv(file_name,mode='a',header=False,index=False)
 
-def clerk_page(window,locality):
-    #clerk_db=pd.read_csv("File_name.csv")
-    clerk_file="new_"+locality+".csv"
-    prob_type=StringVar()
+def clerk_page(window,Database,locality):
+    new_complaints_df = pd.read_csv('https://drive.google.com/uc?id='+Database[2]["new "+locality])
+    prob_type = StringVar()
     prob_type.set("--")
-    street_name=StringVar()
+    street_name = StringVar()
     street_name.set("")
     def register_entries():
-        entry_list=[{"Locality":str(locality),"Street":street_name.get(),"Problem":prob_type.get(),"Reporting Date":str(date.today())}]
-        entry_df=pd.DataFrame(entry_list)
-        commit_changes(clerk_file,entry_df)
+        entry_list = [{"Locality":str(locality),"Street":street_name.get(),"Problem":prob_type.get(),"Reporting Date":str(date.today())}]
+        nonlocal new_complaints_df
+        new_complaints_df = pd.concat([new_complaints_df,pd.DataFrame(entry_list)], ignore_index=True)
         prob_type.set('--')
-        street_name.set('')        
-    clerk_frame=Frame(window)
-    Label(clerk_frame,text="Choose the type of Problem ",font=('Courier New Greek',18)).pack(pady=20)
-    prob_menu=OptionMenu(clerk_frame,prob_type,"Pothole","Broken Curb","Others")
-    prob_menu.config(font=('Courier New Greek',15),width=18)
-    prob_menu.pack(anchor=CENTER,padx=70)
-    Label(clerk_frame,text="Enter the Street Name :",font=('Courier New Greek',18)).pack(pady=15)
-    street_entry=Entry(clerk_frame,textvariable=street_name,font=('Courier New Greek',15))
+        street_name.set('')
+    clerk_frame = Frame(window)
+    Label(clerk_frame, text="Choose the type of Problem ", font=('Courier New Greek',18)).pack(pady=20)
+    prob_menu = OptionMenu(clerk_frame,prob_type,"Pothole","Broken Curb","Others")
+    prob_menu.config(font=('Courier New Greek',15), width=18)
+    prob_menu.pack(anchor=CENTER, padx=70)
+    Label(clerk_frame, text="Enter the Street Name :", font=('Courier New Greek',18)).pack(pady=15)
+    street_entry = Entry(clerk_frame, textvariable=street_name, font=('Courier New Greek',15))
     street_entry.pack()
-    Button(clerk_frame,text="Register", font=('Poppins bold', 18),command=register_entries).pack()
+    Button(clerk_frame, text="Register", font=('Poppins bold', 18), command=register_entries).pack()
     clerk_frame.pack()
+
+    def exit():
+        new_complaints_df.to_csv('temp.csv', index=False)
+        file_obj = Database[0].CreateFile({'parents': [{'id': Database[1]}], 'id': Database[2]['new '+locality]})
+        file_obj.SetContentFile(filename='temp.csv')
+        file_obj.Upload()
+        clerk_frame.destroy()
+    Button(clerk_frame, text="Log Out", command=exit).pack()
---
+++ Login.py
@@ -0,0 +1,38 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import pandas as pd
+from tkinter import *
+import Clerk
+import Supervisor
+
+gauth = GoogleAuth()
+gauth.LoadCredentialsFile("credentials.json")
+if gauth.credentials is None:
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    gauth.Refresh()
+else:
+    gauth.Authorize()
+
+gauth.SaveCredentialsFile("credentials.json")
+
+drive = GoogleDrive(gauth)
+
+database_folder = '1H5CUI-DRExlAleJwr0P_jlwh5c_GUJem'
+links_file = '1i-PIzY2z8a0V7QKijE5zBI4bqPng749O'
+links_df = pd.read_csv('https://drive.google.com/uc?id='+links_file)
+database_file = {}
+for col in links_df.columns:
+    database_file[col] = links_df[col][0]
+
+Database = (drive,database_folder,database_file)
+
+window=Tk()
+window.rowconfigure(0, weight=1)
+window.columnconfigure(0, weight=1)
+window.state('zoomed')
+window.resizable(0, 0)
+window.title('Road Repair and Tracking Software')
+#Clerk.clerk_page(window,Database,"Andheri")
+Supervisor.supervisor_page(window,Database,"Andheri")
+window.mainloop()
